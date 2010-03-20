a380591efabd0fb1f075be0ab8699fcbcf39c776
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-20 15:23:05 +0000

    New exceptions

---
+++ groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/utils/exceptions/BadPlaceException.java
@@ -0,0 +1,14 @@
+package fr.alma.atarigo.utils.exceptions;
+
+public class BadPlaceException extends Exception {
+
+	public BadPlaceException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7651675431627421683L;
+
+}
