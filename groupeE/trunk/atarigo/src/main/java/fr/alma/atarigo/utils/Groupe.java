316ba2690e1dbdfe1c2c3d8fd9e8b4b082f93180
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-21 00:13:02 +0000

    Almost working game

--- groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/utils/Groupe.java
+++ groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/utils/Groupe.java
@@ -5,66 +5,64 @@ import java.util.HashSet;
 import fr.alma.atarigo.utils.exceptions.BadCouleurException;
 
 public class Groupe {
-	
-	private HashSet<Pion> pions;
-        private HashSet<Pion> yeux;
-        private int libertes;
-	private PionVal couleur;
-	
-	
-	public Groupe(PionVal couleur) {
-		super();
-		this.couleur = couleur;
-		pions = new HashSet<Pion>();
-                yeux = new HashSet<Pion>();
-	}
-	
-	public boolean contains(Pion p){
-		return pions.contains(p);
-	}
-	
-	public void addPion(Pion p) throws BadCouleurException {
-		if (p.getCouleur() == couleur){
-			this.pions.add(p);
-		} else {
-			throw new BadCouleurException("Ce groupe est de couleur "+couleur.valeur());
-		}
-	}
 
-	
-	public boolean intersectionNonVide(Groupe autre){
-		if(!couleur.equals(autre.getCouleur())) {
-			return false;
-		}
-		for(Pion p:pions){
-			if(autre.contains(p)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public Object getCouleur() {
-		return couleur;
-	}
+    private HashSet<Pion> pions;
+    private HashSet<Pion> yeux;
+    private int libertes;
+    private PionVal couleur;
 
-	public Groupe fusion(Groupe autre){
-		Groupe sortie = null;
-		if(this.intersectionNonVide(autre)){
-			//Si on partage un pion, alors on doit fusionner les deux groupes.
-			sortie = new Groupe(couleur);
-			sortie.pions.addAll(pions);
-			sortie.pions.addAll(autre.pions);
-		}
-		
-		return sortie;
-	}
+    public Groupe(PionVal couleur) {
+        super();
+        this.couleur = couleur;
+        pions = new HashSet<Pion>();
+        yeux = new HashSet<Pion>();
+    }
 
-	public void addAll(Groupe autre) {
-		if(this.getCouleur() == autre.getCouleur()){
-			this.pions.addAll(autre.pions);
-		}
-	}
+    public boolean contains(Pion p) {
+        return pions.contains(p);
+    }
+
+    public void addPion(Pion p) throws BadCouleurException {
+        if (p.getCouleur() == couleur) {
+            this.pions.add(p);
+        } else {
+            throw new BadCouleurException("Ce groupe est de couleur " + couleur.valeur());
+        }
+    }
+
+    public boolean intersectionNonVide(Groupe autre) {
+        if (!couleur.equals(autre.getCouleur())) {
+            return false;
+        }
+        for (Pion p : pions) {
+            if (autre.contains(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Object getCouleur() {
+        return couleur;
+    }
+
+    public Groupe fusion(Groupe autre) {
+        Groupe sortie = null;
+        if (this.intersectionNonVide(autre)) {
+            //Si on partage un pion, alors on doit fusionner les deux groupes.
+            sortie = new Groupe(couleur);
+            sortie.pions.addAll(pions);
+            sortie.pions.addAll(autre.pions);
+        }
+
+        return sortie;
+    }
+
+    public void addAll(Groupe autre) {
+        if (this.getCouleur() == autre.getCouleur()) {
+            this.pions.addAll(autre.pions);
+        }
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -100,12 +98,16 @@ public class Groupe {
         return libertes;
     }
 
-    Iterable<Pion> getPions() {
+    public void setLibertes(int libertes) {
+        this.libertes = libertes;
+    }
+
+    public Iterable<Pion> getPions() {
         return pions;
     }
-	
-	
-	
-	
+
+    public int size(){
+        return pions.size();
+    }
 
 }
