337e7db2ec00bb59522589c94cd980276e6eb7fe
manoel.fortun <manoel.fortun@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-24 21:01:43 +0000

    Pom->java 1.6 Correction algoConstrution de l'arbre de coup Algo Parcours de l'arbre pour évaluation terminé.

--- GroupeK/trunk/src/main/java/fr/alma/modele/intelligence/SunTsu.java
+++ GroupeK/trunk/src/main/java/fr/alma/modele/intelligence/SunTsu.java
@@ -1,5 +1,6 @@
 package fr.alma.modele.intelligence;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import fr.alma.modele.Coordonnee;
@@ -15,15 +16,13 @@ import fr.alma.modele.arbres.Node;
 
 
 /**
- * Class qui gère l'intelligence articificiel
+ * Classe qui gère l'intelligence articificiel
  * 
  */
 public class SunTsu {
 
 	private CouleurPion coul;
 	private Difficulte diff;
-	//FIXME discuter avec Bambinome pour savoir si on représente des matrice de pion 
-	//ou des go ban, voir eventuellement une troisième classe plus complete (pour gestion des groupes etc).
 	private Arbre<Coup> bibliotheque;
 	private GoBan situation;
 	
@@ -39,7 +38,7 @@ public class SunTsu {
 		this.situation=situation;
 		
 		//diff.ordinal()*5;
-		constructionArbre(6,bibliotheque.racine());
+		constructionArbre(6,bibliotheque.racine(), coul);
 		
 		
 		/*
@@ -56,21 +55,13 @@ public class SunTsu {
 		return 0;
 	}
 
-	private void constructionArbre(int profondeur, Node<Coup> noeud){
+	private void constructionArbre(int profondeur, Node<Coup> noeud, CouleurPion ajouer){
 		try{
 			int compteurFils=1;
 			Pion[][] matrice=situation.getGoban();
 			for(int i=0; i<9;i++){
 				for(int j=0; j<9;j++){
-					//TODO revoir le choix de la couleur en fonction de la profondeur
-					//rajouter un paramètre pour déterminer le tour de qui c'est.
-					//même chose à faire pour la notation de l'arbre.
-					
-					
-					//le modulo 2 indique si on est en impair ou pair, donc la couleur du joueur
-					//so impair c'est le joueur pair l'autre.
-					CouleurPion couleurPion=profondeur%2==1?CouleurPion.BLANC:CouleurPion.NOIR;
-					if(matrice[i][j].getCouleur()==CouleurPion.EMPTY&& situation.estLegal(i, j, couleurPion)){
+					if(matrice[i][j].getCouleur()==CouleurPion.EMPTY&& situation.estLegal(i, j, ajouer)){
 						/*
 						 * On ajoute le pion si la case est vide et que le coup est légal
 						 * on ajoute le pion et on enregistre le coup dans l'arbre
@@ -78,17 +69,17 @@ public class SunTsu {
 						 * de l'arbre
 						 * sinon on descend dans l'arbre.
 						 */
-						situation.ajouterPion(i, j,coul);						
-						Coup coup= new Coup(i, j, couleurPion);
+						situation.ajouterPion(i, j,ajouer);						
+						Coup coup= new Coup(i, j, ajouer);
 						Node<Coup> node=bibliotheque.getNewNode();
 						node.setValeur(coup);
 						node.setEtiquette("feuille");
 						bibliotheque.ajouterFils(noeud, node, compteurFils);
 						compteurFils++;
 						if(profondeur>1){
-							constructionArbre(profondeur-1, node);
+							constructionArbre(profondeur-1, node,inverseCoul(ajouer));
 						}
-						situation.retirerPion(i, j, coul);
+						situation.retirerPion(i, j, ajouer);
 					}
 				}
 			}
@@ -98,7 +89,7 @@ public class SunTsu {
 	}
 	
 	
-	private Coup notationArbre(int profondeur, Node<Coup> noeud){
+	private Coup notationArbre(int profondeur, Node<Coup> noeud, CouleurPion ajouer){
 		/*Algo de notation de l'arbre:
 		 * 
 		 * descend jusqu'a la profondeur max
@@ -113,37 +104,160 @@ public class SunTsu {
 		 * 
 		 * 
 		 */
-		CouleurPion coulp= coul;
+		
 		
 		try{
 		Node<Coup> noeudCoup;
 		Coup cp;
 		LinkedList<Coup> notes= new LinkedList<Coup>();
-		if (profondeur==1){
+		/*
+		 * On descend dans l'arbre en parcourant tout les fils
+		 * une fois arrivé au plus bas
+		 * on calcul les valeurs des situations 
+		 * et on remonte la note
+		 * et on redescent et calculer les notes
+		 * une fois toute les notes on fait le min max
+		 * 
+		 */
 			for (int i=1; i<=bibliotheque.getArrite(); i++){
 				noeudCoup=bibliotheque.iemeFils(noeud, i);
 				if( noeudCoup!=null&& ((cp=noeudCoup.getValeur())!=null)){
-					
 					situation.ajouterPion(cp.getPosition().getX(), cp.getPosition().getY(), cp.getCoulp());
-					cp.setNote(conseille(coulp));
+					if (profondeur==1){
+						cp.setNote(conseille(ajouer));
+						notes.add(cp);
+					}else{
+					notes.add(notationArbre(profondeur-1,noeudCoup , inverseCoul(ajouer)));
+					}
 					situation.retirerPion(cp.getPosition().getX(), cp.getPosition().getY(), cp.getCoulp());
-					notes.add(cp);
+					
 				}
 			}
-			//TODO a finir
+			cp=minMaxer(notes, ajouer);
+			if (cp==null){
+				noeud.getValeur().setNote(conseille(ajouer));
+			}else
+			{
+				noeud.getValeur().setNote(cp.getNote());
+			}
+			return noeud.getValeur();
 			
-			
-		}else{
-			
-		}
+		
 		
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
-		
-		
 		return null;
 	}
+	
+	
+	private Coup notationArbreAlphaBeta(int profondeur, Node<Coup> noeud, CouleurPion ajouer){
+		/*Algo de notation de l'arbre:
+		 * 
+		 * descend jusqu'a la profondeur max
+		 * on evalue chaque noeud
+		 * on supprime tout les fils
+		 * on remonte la note
+		 * on redescend dans chaque fils
+		 * une fois qu'on remonte on compare toutes les notes
+		 * avant de remonter et decsendre.
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		try{
+		Node<Coup> noeudCoup;
+		Coup cp;
+		LinkedList<Coup> notes= new LinkedList<Coup>();
+		/*
+		 * On descend dans l'arbre en parcourant tout les fils
+		 * une fois arrivé au plus bas
+		 * on calcul les valeurs des situations 
+		 * et on remonte la note
+		 * et on redescent et calculer les notes
+		 * une fois toute les notes on fait le min max
+		 * 
+		 */
+			for (int i=1; i<=bibliotheque.getArrite(); i++){
+				noeudCoup=bibliotheque.iemeFils(noeud, i);
+				if( noeudCoup!=null&& ((cp=noeudCoup.getValeur())!=null)){
+					situation.ajouterPion(cp.getPosition().getX(), cp.getPosition().getY(), cp.getCoulp());
+					if (profondeur==1){
+						cp.setNote(conseille(ajouer));
+						notes.add(cp);
+					}else{
+					notes.add(notationArbre(profondeur-1,noeudCoup , inverseCoul(ajouer)));
+					}
+					situation.retirerPion(cp.getPosition().getX(), cp.getPosition().getY(), cp.getCoulp());
+					
+				}
+			}
+			cp=minMaxer(notes, ajouer);
+			if (cp==null){
+				noeud.getValeur().setNote(conseille(ajouer));
+			}else
+			{
+				noeud.getValeur().setNote(cp.getNote());
+			}
+			return noeud.getValeur();
+			
+		
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	public CouleurPion inverseCoul(CouleurPion col){
+		if (col.equals(CouleurPion.BLANC)){
+			return CouleurPion.NOIR;
+		}else{
+			return CouleurPion.BLANC;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	private Coup minMaxer(LinkedList<Coup> list, CouleurPion ajouer){
+		Coup result=null;
+		Iterator<Coup> ieter=list.iterator();
+		Coup temp=null;
+		if( coul==ajouer){
+			while(ieter.hasNext()){
+				temp=ieter.next();
+				if( result==null){
+					result=temp;
+				}else{
+					if(temp!=null){
+					result=result.getNote()<temp.getNote()?result:temp;
+					}
+				}
+			}
+		}else{
+			while(ieter.hasNext()){
+				temp=ieter.next();
+				if( result==null){
+					result=temp;
+				}else{
+					if(temp!=null){
+					result=result.getNote()>temp.getNote()?result:temp;
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	
 	
 }
