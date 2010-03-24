4f4b3ccac3c5ad18e1516f2eebf9a516532ff432
freddumonceaux@yahoo.fr <freddumonceaux@yahoo.fr@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-24 11:21:44 +0000

    Import du repertoire

---
+++ GroupeC/trunk/Main/src/go/Tree.java
@@ -0,0 +1,59 @@
+package go;
+
+import java.util.LinkedList;
+
+/**
+ * @author Frédéric Dumonceaux
+ *
+ */
+
+
+public class Tree {
+	
+	
+
+	public Tree(){
+	
+	}
+	
+
+	public Tree(Move m){
+		
+	}
+	
+	
+	public Tree creerArbre(Node n){
+		
+	}
+
+
+	public Tree ajouterFils(Node n){
+		
+	}
+
+
+	public Tree ajouterFils(Node child, Node parent){
+		
+	}
+	
+ 	
+ 	public Tree racine(){
+		
+	}
+ 	
+ 	
+	public Node rootNode(){
+		return this.root;
+	}
+ 	
+	
+	public Node node(Move m){
+		return root.node(m);
+	}
+ 	
+ 	
+ 	public Node pere(Node n){
+		return n.pere;
+	}
+ 	
+}
