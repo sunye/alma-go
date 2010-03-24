b94581232dc56ec045a2f37161b1b32f474a3830
freddumonceaux@yahoo.fr <freddumonceaux@yahoo.fr@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-24 11:21:44 +0000

    Import du repertoire

---
+++ GroupeC/trunk/Main/src/go/Move.java
@@ -0,0 +1,73 @@
+package go;
+
+/**
+ * @author Frédéric Dumonceaux
+ *
+ */
+
+
+public class Move {
+	
+
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Move(byte x, byte y) {
+
+	}
+	
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Move(int x, int y, byte color) {
+
+	}
+	
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public void setMove(int x, int y, byte color) {
+
+
+	}
+
+
+	/**
+	 * @param evaluate the evaluate to set
+	 */
+	public void setEvaluate(boolean evaluate) {
+
+	}
+
+
+	/**
+	 * @return the evaluate
+	 */
+	public boolean isEvaluate() {
+
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+
+	}
+}
---
+++ GroupeC/trunk/Main/src/go/Node.java
@@ -0,0 +1,30 @@
+package go;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author Frédéric Dumonceaux
+ *
+ */
+
+
+public class Node{
+
+
+	
+	
+	public Node(Move m){
+
+	}
+	
+	
+	public Node node(Move m){
+
+	}
+	
+	
+	public void ajouterFils(Node f){
+
+	}
+}
