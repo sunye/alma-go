0b42442048fb11d316b12ff76dc0fc959b876997
adrien.guille <adrien.guille@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-27 15:17:27 +0000

    Advanced pom.xml and new english test class

---
+++ groupeD/trunk/src/test/java/fr/alma/TestTree.java
@@ -0,0 +1,63 @@
+package fr.alma.test;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+
+import fr.alma.atarigo.Goban;
+import fr.alma.ia.Tree;
+import junit.framework.TestCase;
+
+public class TestTree extends TestCase {
+
+	public void testAddChild() {
+		Goban goban = new Goban(1,1);
+		Tree tree = new Tree(goban);
+		Tree child = new Tree(goban);
+		
+		tree.addChild(child);
+		
+		Field field;
+		try{
+			field = tree.getClass().getDeclaredField("children_");
+			//L'attribut étant privé, on le rend lisible
+			field.setAccessible(true);
+			//On récupère la pile devant être modifiée durant l'exécution
+			LinkedList<Tree> list = (LinkedList<Tree>) field.get(tree);
+			
+			assertEquals(list.size(),1);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public void testIsLeaf() {
+		Goban goban = new Goban(1,1);
+		Tree tree = new Tree(goban);
+		Tree child = new Tree(goban);
+		
+		tree.addChild(child);
+		
+		assertTrue(child.isLeaf());
+		assertFalse(tree.isLeaf());
+	}
+	
+	public void testGetParent() {
+		Goban goban = new Goban(1,1);
+		Tree tree = new Tree(goban);
+		Tree child = new Tree(goban);
+		
+		assertEquals(tree.getParent(),null);
+		tree.addChild(child);
+		assertNotNull(child.getParent());
+	}
+
+	public void testGetChildren() {
+		Goban goban = new Goban(1,1);
+		Tree tree = new Tree(goban);
+		assertNotNull(tree.getGoban());
+	}
+
+	public void testGenerateChildren() {
+		//fail("Not yet implemented");
+	}
+}
