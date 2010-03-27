0de91c91d97865fcecffd35e8a44ee20ed3357e8
ferreira.vincent14 <ferreira.vincent14@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-27 16:12:53 +0000

    interface improvement. better vision of the game

--- groupeD/trunk/src/main/java/fr/alma/ui/InfoPanel.java
+++ groupeD/trunk/src/main/java/fr/alma/ui/InfoPanel.java
@@ -2,62 +2,102 @@ package fr.alma.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class InfoPanel extends JPanel {
 
 	private JLabel title;
-	private JPanel container;
-	private JPanel challengeContainer;
 	private JLabel challengeTitle;
 	private JLabel challengeNb;
 	private JPanel blackAndWhiteInfos;
-	private JLabel capturedTitle1;
-	private JLabel capturedTitle2;
-	JLabel labelBlack = new JLabel("NOIR",JLabel.CENTER);
-	JLabel labelWhite = new JLabel("BLANC",JLabel.CENTER);
+	private JLabel capturedBlack;
+	private JLabel capturedWhite;
+	private JLabel difficultyTitle;
+	private JLabel difficulty;
 	
 	
 	
 	
 	public void buildAll(){
+		
 		title = new JLabel("Panneau d'infos");
 		
-		challengeContainer = new JPanel();
-		challengeContainer.setLayout(new BorderLayout());
-		challengeTitle = new JLabel("Nombre de captures pour gagner");
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gBC = new GridBagConstraints();
+        gBC.fill = GridBagConstraints.HORIZONTAL;
+		// titre "Capture pour gagner"
+		challengeTitle = new JLabel("Captures pour gagner");
+		gBC.ipady = 40;
+		gBC.weightx = 0.5;
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        add(challengeTitle, gBC);
+        
+        // nombre de captures pour gagner
 		challengeNb = new JLabel("0");
-		challengeContainer.add(challengeTitle,BorderLayout.WEST);
-		challengeContainer.add(challengeNb,BorderLayout.EAST);
-		blackAndWhiteInfos = new JPanel();
-		GridLayout gridLayout1 = new GridLayout(4, 2);
-		blackAndWhiteInfos.setLayout(gridLayout1);
-		capturedTitle1 = new JLabel("Captures");
-		capturedTitle2 = new JLabel("Captures");
-		//labelBlack.setIcon(new ImageIcon(this.getClass().getResource("/fr/alma/resources/images/black-stone.gif")));
-		//labelWhite.setIcon(new ImageIcon(myApp.getClass().getResource("/fr/alma/resources/images/white-stone.gif")));
-		blackAndWhiteInfos.add(capturedTitle1);
-		blackAndWhiteInfos.add(capturedTitle2);
-		
-		container = new JPanel();
-		container.setBackground(Color.white);
-        container.setLayout(new BorderLayout());
+        gBC.gridx = 2;
+        gBC.gridy = 0;
+        add(challengeNb, gBC);
         
-        
-        container.add(challengeContainer, BorderLayout.NORTH);
-        container.add(blackAndWhiteInfos, BorderLayout.CENTER);
-        
-        
-        //container.add(bouton, BorderLayout.SOUTH);
+		// titre "vies restantes"
+		challengeTitle = new JLabel("vies restantes");
+		challengeTitle.setHorizontalTextPosition(JLabel.RIGHT);
+		gBC.ipady = 20;
+		gBC.weightx = 0.5;
+		gBC.gridwidth = 4;
+        gBC.gridx = 0;
+        gBC.gridy = 1;
+        gBC.fill = gBC.CENTER;
+        add(challengeTitle, gBC);
 
-		
-		add(title);
-		add(container);
-		
+        capturedBlack = new JLabel("0");
+        capturedBlack.setIcon(new ImageIcon(getClass().getResource("/fr/alma/resources/images/black-stone.gif")));
+        gBC.gridx = 0;
+        gBC.gridy = 2;
+        gBC.fill = gBC.HORIZONTAL;
+        add(capturedBlack, gBC);
+        
+		capturedWhite = new JLabel("0");
+		capturedWhite.setIcon(new ImageIcon(getClass().getResource("/fr/alma/resources/images/white-stone.gif")));
+		gBC.gridx = 2;
+        gBC.gridy = 2;
+        gBC.ipady = 40;
+        add(capturedWhite, gBC);
+        
+        /*
+		// titre "Niveau du joueur"
+		challengeTitle = new JLabel("Captures pour gagner");
+		gBC.weightx = 0.5;
+        gBC.gridx = 0;
+        gBC.gridy = 3;
+        add(challengeTitle, gBC);
+        
+        // nombre de captures pour gagner
+		challengeNb = new JLabel("0");
+        gBC.gridx = 2;
+        gBC.gridy = 3;
+        add(challengeNb, gBC);
+        */
+        
+	}
+	
+	public void setChallenge(int n){
+		challengeNb.setText(String.valueOf(n));
+	}
+	
+	public void setBlackLife(int n){
+		capturedBlack.setText(String.valueOf(n));
+	}
+	
+	public void setWhiteLife(int n){
+		capturedWhite.setText(String.valueOf(n));
 	}
 	
 	
