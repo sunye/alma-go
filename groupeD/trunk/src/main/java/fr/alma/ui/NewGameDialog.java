82c7d96afd1f7b2618e58d848039b11445acf65e
ferreira.vincent14 <ferreira.vincent14@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-28 15:09:45 +0000

    UI improvement : now we can choose the IA player difficulty from 1 to 4 which is in fact the tree depth number

--- groupeD/trunk/src/main/java/fr/alma/ui/NewGameDialog.java
+++ groupeD/trunk/src/main/java/fr/alma/ui/NewGameDialog.java
@@ -37,14 +37,14 @@ public class NewGameDialog extends JDialog {
 	public NewGameDialog(JFrame parent, String title, boolean modal,MyApplication myApp){
 		super(parent, title, modal);
 		this.myApp = myApp;
-		this.setSize(250, 280);
+		this.setSize(250, 320);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.initComponent();
 	}
 	JPanel jpan0 = new JPanel();
 	JPanel jpan1 = new JPanel();
-	GridLayout gridLayout1 = new GridLayout(4, 2);
+	GridLayout gridLayout1 = new GridLayout(5, 2);
 	JLabel labelChallenge = new JLabel("Prises min");
 	JLabel labelBlack = new JLabel("NOIR",JLabel.CENTER);
 	JLabel labelWhite = new JLabel("BLANC",JLabel.CENTER);
@@ -53,6 +53,10 @@ public class NewGameDialog extends JDialog {
 	JButton goButton = new JButton("Go !");
     JButton quitButton = new JButton("Quitter");
     SpinnerNumberModel model1 = new SpinnerNumberModel(1,1,10,1);
+    SpinnerNumberModel dif1 = new SpinnerNumberModel(1,1,4,1);
+    SpinnerNumberModel dif2 = new SpinnerNumberModel(1,1,4,1);
+    JSpinner spinnerDif1 = new JSpinner(dif1);
+    JSpinner spinnerDif2 = new JSpinner(dif2);
     JSpinner spinner1 = new JSpinner(model1);
 
 	/**
@@ -67,21 +71,53 @@ public class NewGameDialog extends JDialog {
 		labelWhite.setIcon(new ImageIcon(myApp.getClass().getResource("/fr/alma/resources/images/white-stone.gif")));
 		labelWhite.setVerticalTextPosition(JLabel.NORTH);
 		labelWhite.setHorizontalTextPosition(JLabel.CENTER);
-		combo1.addItem("Humain");
 		combo1.addItem("Ordinateur");
-        combo2.addItem("Humain");
+		combo1.addItem("Humain");
         combo2.addItem("Ordinateur");
+        combo2.addItem("Humain");
+        combo1.addActionListener(new StratListener());
+        combo2.addActionListener(new StratListener());
         jpan1.add(labelChallenge);
         jpan1.add(spinner1);
 		jpan1.add(labelBlack);
 		jpan1.add(labelWhite);
 		jpan1.add(combo1);
 		jpan1.add(combo2);
+		jpan1.add(spinnerDif1);
+		jpan1.add(spinnerDif2);
 		jpan1.add(goButton);
 		jpan1.add(quitButton);
 		goButton.addActionListener(new GoListener());             
 		quitButton.addActionListener(new QuitListener());
 		this.getContentPane().add(jpan1);
+	}
+	
+	public class StratListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JComboBox cb = (JComboBox)e.getSource();
+			System.out.println("eloo");
+	        if(cb.getSelectedItem().equals("Humain")){
+	        	if(cb.equals(combo1)){
+	        		jpan1.remove(spinnerDif1);
+	        	}
+	        	else{
+	        		jpan1.remove(spinnerDif2);
+	        	}
+	        }
+	        else{
+	        	if(cb.equals(combo1)){
+	        		jpan1.add(spinnerDif1);
+	        		System.out.println("ajout");
+	        	}
+	        	else{
+	        		jpan1.add(spinnerDif2);
+	        	}
+	        }
+
+		}
+		
 	}
 	
 	/**
@@ -141,13 +177,15 @@ public class NewGameDialog extends JDialog {
     		player1 = new HumanPlayer(Stone.BLACK,"bob");
     	}
     	else{
-    		player1 = new IAPlayer(Stone.BLACK,"bob");
+    		int difIA1 = dif1.getNumber().intValue();
+    		player1 = new IAPlayer(Stone.BLACK,"bob",difIA1);
     	}
     	if(combo2.getSelectedItem().toString().equals("Humain")){
     		player2 = new HumanPlayer(Stone.WHITE,"obi");
     	}
     	else{
-    		player2 = new IAPlayer(Stone.WHITE,"obi");
+    		int difIA2 = dif2.getNumber().intValue();
+    		player2 = new IAPlayer(Stone.WHITE,"obi",difIA2);
     	}
     	myApp.panInfos.setChallenge(model1.getNumber().intValue());
     	myApp.panInfos.setBlackLife(model1.getNumber().intValue());
