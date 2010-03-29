c83807d9f3ae3cb75f7e117bb71cac76ff424f5e
julien.sambre <julien.sambre@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-29 11:56:53 +0000

    ajout du fichier alphaBeta

---
+++ GroupeL/trunk/src/main/java/ia/alphaBeta.java
@@ -0,0 +1,53 @@
+package ia;
+
+public class alphaBeta {
+	
+	/*
+	 * Algorithme Alpha-Beta
+	 * 
+	 * fonction ALPHABETA(P, alpha, beta)  alpha est toujours inferieur à beta 
+     * si P est une feuille alors
+     *  retourner la valeur de P
+     * sinon
+     *  si P est un noeud Min alors
+     *      Val = infini
+     *      pour tout fils Pi de P faire
+     *          Val = Min(Val, ALPHABETA(Pi, alpha, beta))                
+     *          si alpha >= Val alors   coupure alpha 
+     *              retourner Val
+     *          beta = Min(beta, Val)
+     *      finpour            
+     *  sinon
+     *      Val = -infini
+     *      pour tout fils Pi de P faire
+     *          Val = Max(Val, ALPHABETA(Pi, alpha, beta))                
+     *          si Val >= beta alors  coupure beta 
+     *              retourner Val
+     *          alpha = Max(alpha, Val)
+     *      finpour
+     *  finsi
+     * retourner Val
+     * finsi
+     * fin
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
