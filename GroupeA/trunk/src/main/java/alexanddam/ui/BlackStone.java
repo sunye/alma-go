a52589bf6285653496aa09b68fa0eb4c945eefcc
damien.chaillou <damien.chaillou@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-03-30 13:40:06 +0000

    Correction des bugs et amélioration de la fonction d'évaluation

---
+++ GroupeA/trunk/src/main/java/alexanddam/ui/BlackStone.java
@@ -0,0 +1,32 @@
+package main.java.alexanddam.ui;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+
+public class BlackStone extends Canvas{
+	private static final long serialVersionUID = 1L;
+	Image image;
+
+	String img = "black.gif";
+	
+	public BlackStone() {
+		MediaTracker media = new MediaTracker(this);
+		image = Toolkit.getDefaultToolkit().getImage(img);
+		media.addImage(image, 0);
+		try {
+			media.waitForID(0); 
+		}
+		catch (Exception e) {}
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(image, 0,0, this);
+	}
+
+	public Image getImage() {
+		return image;
+	}
+}
