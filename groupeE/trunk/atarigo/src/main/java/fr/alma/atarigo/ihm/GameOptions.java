bc545f56506bc84f34f493129c6e3738ef73b51f
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-04 21:51:39 +0000

    Options, not working

---
+++ groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/ihm/GameOptions.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.alma.atarigo.ihm;
+
+import fr.alma.atarigo.utils.PionVal;
+
+/**
+ *
+ * @author judu
+ */
+public class GameOptions {
+
+    private int nbPlayer;
+
+    public GameOptions() {
+    }
+
+
+    
+
+    public int getNbPlayer() {
+        return nbPlayer;
+    }
+
+    public void setNbPlayer(int nbPlayer) {
+        this.nbPlayer = nbPlayer;
+    }
+
+
+
+    
+
+
+}
