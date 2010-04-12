58452f6c8ffebdc59ab1eda89366873b946bf6d2
alexandre.garnier.86 <alexandre.garnier.86@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-12 16:23:16 +0000

    Stupid bug away...

--- GroupeM/trunk/src/fr/alma/go/Game.java
+++ GroupeM/trunk/src/fr/alma/go/Game.java
@@ -1,42 +1,13 @@
 package fr.alma.go;
 
-import fr.alma.go.goban.Goban;
-import fr.alma.go.interfaces.IPlayer;
-import fr.alma.go.player.Computer;
-import fr.alma.go.player.Human;
-
 public class Game {
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		Goban goban = new Goban();
-		IPlayer black=new Human('b');
-		IPlayer white=new Computer('w',1);
-		boolean turn = true; // true for black turn, false for white turn
-		while (true) {
-			if (turn) {
-				int place = black.getPlace();
-				int abs = place / 10;
-				int ord = place % 10;
-				goban.play(abs, ord, 'b');
-			} else {
-				int place = white.getPlace();
-				int abs = place / 10;
-				int ord = place % 10;
-				goban.play(abs, ord, 'w');
-			} // if
-			if (goban.gameOver()) {
-				if (turn) {
-					// TODO black wins
-				} else {
-					// TODO white wins
-				} // if
-				break;
-			}
-			turn = !turn;
-		} // while
+	}
 
-	} // static void main(String[])
-} // class Game
+}
