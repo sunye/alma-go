7ae34ec8a587b548d96eead31530b5775971c843
adrien.guille <adrien.guille@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-13 13:33:23 +0000

    new test classes

--- groupeD/trunk/src/test/java/fr/alma/TestTree.java
+++ groupeD/trunk/src/test/java/fr/alma/TestTree.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 import fr.alma.atarigo.Goban;
+import fr.alma.atarigo.Position;
+import fr.alma.atarigo.Stone;
 import fr.alma.ia.Tree;
 import junit.framework.TestCase;
 
@@ -59,6 +61,30 @@ public class TestTree extends TestCase {
 	}
 
 	public void testGenerateChildren() {
-		//fail("Not yet implemented");
+		Goban goban = new Goban(4,4);
+		Tree tree = new Tree(goban);
+		tree.generateChildren(null, Stone.WHITE, Stone.BLACK);
+		assertEquals(tree.getChildren().size(),16);
+	}
+	
+	public void testGetDepth(){
+		Goban goban = new Goban(4,4);
+		Tree tree = new Tree(goban);
+		Tree tree2 = new Tree(null);
+		assertEquals(tree.getDepht(),0);
+		tree.addChild(tree2);
+		assertEquals(tree2.getDepht(),1);
+	}
+	
+	public void testGetMove(){
+		Goban goban = new Goban(4,4);
+		goban.writeCell(null, new Position(1,1), Stone.WHITE, true);
+		Tree tree = new Tree(null);
+		Tree tree2 = new Tree(goban);
+		Tree tree3 = new Tree(null);
+		tree.addChild(tree2);
+		tree2.addChild(tree3);
+		assertEquals(tree3.getMove(),goban);
+		assertEquals(tree2.getMove(),goban);
 	}
 }
