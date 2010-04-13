77c801ed8262f93c7ce83b20ba6e6b1ec718f222
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-13 22:15:10 +0000

    Tout beau tout propre !! Avec la license

--- groupeE/trunk/atarigo/src/test/java/fr/alma/atarigo/utils/GameTest.java
+++ groupeE/trunk/atarigo/src/test/java/fr/alma/atarigo/utils/GameTest.java
@@ -1,11 +1,27 @@
-
-/*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
- */
+// File GameTest.java
+// Last commited $Date$
+// By $Author$
+// Revision $Rev$
+//
+// Copyright (C) 2010 Clotilde Massot & Julien Durillon
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of this program.
 package fr.alma.atarigo.utils;
-
-//~--- non-JDK imports --------------------------------------------------------
 
 import junit.framework.TestCase;
 
@@ -47,6 +63,3 @@ public class GameTest extends TestCase {
         assertEquals(expResult, pion);
     }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
