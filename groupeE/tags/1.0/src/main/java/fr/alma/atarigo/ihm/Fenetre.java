a1c592b920983e870c1e860084340c53e270dd7b
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-13 22:54:08 +0000

    Version rendu à Philippe Martin

---
+++ groupeE/tags/1.0/src/main/java/fr/alma/atarigo/ihm/Fenetre.java
@@ -0,0 +1,155 @@
+/**
+ * File {Fenetre.java}
+ * Last commited $Date$
+ * By $Author$
+ * Revision $Rev$
+ *
+ * Copyright (C) 2010 Clotilde Massot & Julien Durillon
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of this program.
+ */
+
+
+/*
+ * This file has been originaly developed by Anthony Caillaud. Thanks to him for sharing.
+ */
+package fr.alma.atarigo.ihm;
+
+import fr.alma.atarigo.GameManager;
+import fr.alma.atarigo.utils.PionVal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+/**
+ * This class describes the main window of the application. It defines main buttons and actions.
+ * 
+ * @author judu
+ */
+public class Fenetre extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JMenuBar barreMenu = new JMenuBar();
+    private JMenu jeu = new JMenu("Jeu");
+    private JMenu a_propos = new JMenu("À Propos");
+    private JMenu nouv = new JMenu("Nouvelle partie");
+    private JMenuItem item1 = new JMenuItem("1 joueur");
+    private JMenuItem item2 = new JMenuItem("2 joueurs");
+    private JMenuItem propos = new JMenuItem("?");
+    private JMenuItem quitter = new JMenuItem("Quitter");
+    private OptionWindow options;
+    //Panel représentant le goban
+    private GobanPanel pan;
+    private boolean sendData;
+    private GameManager controleur;
+
+    /**
+     * Open a window with the goban and a menu
+     * @param s : Title of the window
+     */
+    public Fenetre(String s) {
+
+        super(s);
+        setSize(750, 680);
+        this.setLocationRelativeTo(null);
+
+
+
+        pan = new GobanPanel();
+        /**
+         * On ajoute le GobanPanel dans la fenetre principale
+         */
+        add(pan);
+        controleur = new GameManager(pan, this);
+
+
+        /**
+         * Mise en place de la barre de menu
+         */
+        //Un joueur :
+        item1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                OptionWindow ow = new OptionWindow(null, "Choisissez votre couleur !", true, controleur);
+            }
+        });
+        this.nouv.add(item1);
+
+        //Deux joueurs :
+        item2.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                controleur.twoPlayers();
+            }
+        });
+        this.nouv.add(item2);
+        this.jeu.add(this.nouv);
+        //Ajout d'un separateur
+        this.jeu.addSeparator();
+
+        //Quitter :
+        quitter.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                System.exit(0);
+            }
+        });
+
+        this.jeu.add(quitter);
+        this.barreMenu.add(jeu);
+
+        //TODO :  changer le "Bla bla bla..." du "A propos" !
+        //A propos :
+        propos.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                JOptionPane jop = new JOptionPane();
+                jop.showMessageDialog(null, "Jeu de Go, version Atari, par Julien Durillon et Clotilde Massot.", "À propos", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        this.a_propos.add(propos);
+        this.barreMenu.add(a_propos);
+
+
+
+        this.setJMenuBar(barreMenu);
+        this.setVisible(true);
+
+        /**
+         *  Arrêt de l'appli quand on clique sur la croix
+         */
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+    }
+
+    /**
+     * Open a window endgame to announce the winner of the game.
+     */
+    public void finJeu() {
+        JOptionPane jop = new JOptionPane();
+            StringBuilder strb = new StringBuilder();
+            strb.append("Le grand gagnant de cette partie est : Joueur ");
+            if (controleur.getGame().getCurrentPlayer() == PionVal.BLANC)
+                strb.append("Noir !");
+            else
+                strb.append("Blanc !");
+            jop.showMessageDialog(null, strb, "Partie terminée !", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ groupeE/tags/1.0/src/main/java/fr/alma/atarigo/utils/Groupe.java
@@ -0,0 +1,205 @@
+// File Groupe.java
+// Last commited $Date$
+// By $Author$
+// Revision $Rev$
+//
+// Copyright (C) 2010 Clotilde Massot & Julien Durillon
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of this program.
+package fr.alma.atarigo.utils;
+
+import java.util.HashSet;
+
+import fr.alma.atarigo.utils.exceptions.BadCouleurException;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * This class describes a group of stones. We have here the stones (all the same color), the eyes, the liberties, and the group's color.
+ * @author judu
+ */
+public class Groupe {
+
+    /// Stones with the same color
+    private Set<Stone> stones;
+    private Set<Groupe> eyes;
+    /// All the free places surrounding the group
+    private Set<Stone> liberties;
+    /// The color of the stones.
+    private PionVal couleur;
+
+
+    /**
+     * We define a group by its color. A colorless group does not mean anything.
+     * @param couleur
+     */
+    public Groupe(PionVal couleur) {
+        super();
+        this.couleur = couleur;
+        stones = new HashSet<Stone>();
+        eyes = new HashSet<Groupe>();
+    }
+
+    /**
+     * Check if the group contains the Stone.
+     * @param p
+     * @return
+     */
+    public boolean contains(Stone p) {
+        return stones.contains(p);
+    }
+
+    /**
+     * Add a Stone to this group, but only a Stone with the same color.
+     * @param p The Stone to add
+     * @throws BadCouleurException
+     */
+    public void addStone(Stone p) throws BadCouleurException {
+        if (p.getCouleur() == couleur) {
+            this.stones.add(p);
+        } else {
+            throw new BadCouleurException("Ce groupe est de couleur " + couleur.valeur());
+        }
+    }
+
+    /**
+     * Checks if the <em>autre</em> Groupe share at least one Stone with <em>this</em>.
+     * @param autre
+     * @return
+     */
+    public boolean intersectionNonVide(Groupe autre) {
+        if (!couleur.equals(autre.getCouleur())) {
+            return false;
+        }
+        for (Stone p : stones) {
+            if (autre.contains(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public PionVal getCouleur() {
+        return couleur;
+    }
+
+
+    /**
+     * Creates a new group containing the union of <em>this</em>, and <em>other</em>.
+     * @param other
+     * @return
+     */
+    public Groupe fusion(Groupe other) {
+        Groupe sortie = null;
+        if (this.intersectionNonVide(other)) {
+            //Si on partage un pion, alors on doit fusionner les deux groupes.
+            sortie = new Groupe(couleur);
+            sortie.stones.addAll(stones);
+            sortie.stones.addAll(other.stones);
+        }
+
+        return sortie;
+    }
+
+    /**
+     * Adds all the stones of <em>other</em> to this.
+     * @param other
+     */
+    public void addAll(Groupe other) {
+        if (this.getCouleur() == other.getCouleur()) {
+            this.stones.addAll(other.stones);
+        }
+    }
+
+    /**
+     * Adds a collection of stones, one-by-one, to ensure all added stones have the same color.
+     * @param collec
+     * @throws BadCouleurException
+     */
+    public void addAll(Collection<Stone> collec) throws BadCouleurException{
+        for(Stone pion:collec){
+            this.addStone(pion);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Groupe other = (Groupe) obj;
+        if (this.stones != other.stones && (this.stones == null || !this.stones.equals(other.stones))) {
+            return false;
+        }
+        if (this.couleur != other.couleur && (this.couleur == null || !this.couleur.equals(other.couleur))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.stones != null ? this.stones.hashCode() : 0);
+        hash = 59 * hash + (this.couleur != null ? this.couleur.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder toStr = new StringBuilder();
+        toStr.append("{ ").append("color : ").append(this.couleur).append(", ");
+        toStr.append("Stones : ").append(stones.toString()).append(", ");
+        toStr.append("Eyes : ").append(eyes).append(" }");
+        return toStr.toString();
+    }
+
+
+
+    public void addEye(Groupe group) {
+        eyes.add(group);
+    }
+
+    public int nbEyes(){
+        return eyes.size();
+    }
+
+    public Set<Groupe> getEyes(){
+        return eyes;
+    }
+
+    public Set<Stone> getLiberties() {
+        return liberties;
+    }
+
+    public void setLiberties(Set<Stone> libertes) {
+        this.liberties = libertes;
+    }
+
+    public Collection<Stone> getStones() {
+        return stones;
+    }
+
+    public int size(){
+        return stones.size();
+    }
+
+}
