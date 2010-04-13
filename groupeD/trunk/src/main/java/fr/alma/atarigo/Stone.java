0efb915ab59c5847f8a705f56b7c70cb122e32c6
ferreira.vincent14 <ferreira.vincent14@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-13 12:51:45 +0000

    update test case

--- groupeD/trunk/src/main/java/fr/alma/atarigo/Stone.java
+++ groupeD/trunk/src/main/java/fr/alma/atarigo/Stone.java
@@ -18,11 +18,8 @@ public enum Stone {WHITE, BLACK, EMPTY;
 	public Stone opponent(){
 		if(this==WHITE){
 			return BLACK;
-		}else{
-			if(this==BLACK)
-				return WHITE;
-			else return EMPTY;
-		}	
+		}else
+			return WHITE;
 	}
 	
 }
