e0594591ef65190a9fd4bd4f7921f2406f5ca46e
alexandre.garnier.86 <alexandre.garnier.86@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-14 08:02:22 +0000

    Long live the heuristics...

--- GroupeM/trunk/src/fr/alma/go/heuristics/GameTree.java
+++ GroupeM/trunk/src/fr/alma/go/heuristics/GameTree.java
@@ -1,10 +1,8 @@
 package fr.alma.go.heuristics;
 
 import java.util.ArrayList;
-
 import fr.alma.go.goban.Goban;
 import fr.alma.go.goban.Place;
-import fr.alma.go.goban.Stone;
 
 public class GameTree {
 
@@ -13,6 +11,8 @@ public class GameTree {
 	private ArrayList<GameTree> sons;
 
 	int[][] coeffs;
+
+	private final static int INFINITE = Integer.MAX_VALUE;
 
 	public GameTree() {
 		root = new GameNode();
@@ -33,13 +33,85 @@ public class GameTree {
 		return root;
 	} // GameNode getRoot()
 
-	private void alphaBetaPruning(Goban goban, boolean color, int deep) {
-		
-	}
+	private int alphaBeta(int alpha, int beta, int deepness) {
+		if (this.isLeaf()) {
+			return root.getNote();
+		} else {
+			if (deepness % 2 == 0) {
+				int note = -INFINITE;
+				for (GameTree son : sons) {
+					note = Math.max(note, son
+							.alphaBeta(alpha, beta, deepness++));
+					if (note >= beta) {
+						root.setNote(note);
+						return note;
+					}
+					alpha = Math.max(alpha, note);
+				} // for
+				root.setNote(note);
+				return note;
+			} else {
+				int note = INFINITE;
+				for (GameTree son : sons) {
+					note = Math.min(note, son
+							.alphaBeta(alpha, beta, deepness++));
+					if (note <= alpha) {
+						root.setNote(note);
+						return note;
+					}
+					beta = Math.min(beta, note);
+				} // for
+				root.setNote(note);
+				return note;
+			} // if
+		} // if
+	} // int alphaBeta(int,int,int)
+
+	private void alphaBetaPruning() {
+		for(GameTree son: sons){
+			son.alphaBeta(INFINITE, -INFINITE, 1);
+		} // for
+	} // void alphaBetaPruning()
 
 	public Place alphaBetaGetPlace(Goban goban, boolean color) {
-		this.alphaBetaPruning(goban, color, 0);
-		return root.getPlace();
-	} // Place alphaBetaPruning(Goban)
+		this.generateTree(goban, color, 3);
+		this.alphaBetaPruning();
+		return this.pickPlace();
+	} // Place alphaBetaGetPlace(Goban)
+
+	private Place pickPlace() {
+		int max = -INFINITE;
+		ArrayList<Place> places = new ArrayList<Place>();
+		for (GameTree son : sons) {
+			if (son.getRoot().getNote() >= max) {
+				if (son.getRoot().getNote() > max) {
+					places.clear();
+				} // if
+				places.add(son.getRoot().getPlace());
+			} // if
+		} // for
+		return places.get((int) Math.random() * places.size());
+	}
+
+	public void generateTree(Goban goban, boolean color, int deepness) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				Goban tmp = goban;
+				if (color ? tmp.play(i, j, 'b') : tmp.play(i, j, 'w')) {
+					GameTree son = new GameTree();
+					son.root.setCoords(i, j);
+					son.root.setNote(coeffs[i][j]);
+					if (deepness > 0) {
+						son.generateTree(tmp, !color, deepness - 1);
+					} // if
+					sons.add(son);
+				} // if
+			} // for
+		} // for
+	} // void generateTree(DummyGoban,boolean,int)
+
+	public boolean isLeaf() {
+		return sons.isEmpty();
+	} // boolean isLeaf()
 
 } // class GameTree
---
+++ GroupeM/trunk/src/fr/alma/go/tests/GameTreeTest.java
@@ -0,0 +1,16 @@
+package fr.alma.go.tests;
+
+import fr.alma.go.goban.Goban;
+import fr.alma.go.heuristics.GameTree;
+import junit.framework.TestCase;
+
+public class GameTreeTest extends TestCase {
+	
+	public void testGenerateTree(){
+		Goban goban=new Goban();
+		GameTree tree=new GameTree();
+		tree.generateTree(goban, true, 0);
+		assertTrue(tree.getSons().get(0).isLeaf());
+	}
+	
+}
