099a0d64efd60cce08e22d0e29b73d893e1dd147
damien.chaillou <damien.chaillou@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-14 11:38:03 +0000

    Nettoyage du code + creation javadoc

--- GroupeA/trunk/src/main/java/alexanddam/logic/Noeud.java
+++
@@ -1,9 +0,0 @@
-package main.java.alexanddam.logic;
-
-abstract class Noeud {
-		
-	int[] vo;				// les differences de l'ordinateur
-	int[] va;				// les differences de l'adversaire
-	int valeur;
-
-}
