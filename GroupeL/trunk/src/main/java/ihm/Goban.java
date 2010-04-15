f8369801383d9d806bfd73e0d061aa47ff479151
julien.sambre <julien.sambre@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-15 17:07:07 +0000

    Update pom.xml

--- GroupeL/trunk/src/main/java/ihm/Goban.java
+++ GroupeL/trunk/src/main/java/ihm/Goban.java
@@ -12,6 +12,26 @@ import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+/*$Author$ 
+ * $Date$ 
+ * $Revision$ 
+ * 
+ *Copyright (C) 2010  Dejean Charles and Pottier Vincent
+ *
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * */
 
 public class Goban extends JPanel{
         
