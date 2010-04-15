d13daf12d55f1cb7153ccfd29d303405a4c86464
lahuidi <lahuidi@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-15 21:44:46 +0000

    Deux test

---
+++ GroupeF/trunk/AtariGO/src/test/java/fr/alma/structure/TestNoeud.java
@@ -0,0 +1,45 @@
+package fr.alma.structure;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+import org.junit.Test;
+import fr.alma.jeu.Pion;
+import fr.alma.structure.Noeud;
+
+public class TestNoeud {
+
+	@Test
+	public void testNoeud() {
+		
+		Noeud noeud = new Noeud(new Pion(new Point(0,0)));
+		
+		assertEquals(noeud.getPere(), null);
+		assertTrue(noeud.getNote() == -1);
+		assertTrue(noeud.getListeFils().size() == 0);
+		assertTrue(noeud.getCoup().position.equals(new Point(0,0)));
+	}
+
+	@Test
+	public void testAjouterFils() {
+		
+		Noeud noeud = new Noeud(new Pion(new Point(0,0)));
+		
+		noeud.AjouterFils(new Noeud(new Pion(new Point(0,1))));	
+		noeud.AjouterFils(new Noeud(new Pion(new Point(0,2))));	
+		
+		assertTrue(noeud.getNbFils() == 2);
+	}
+
+	@Test
+	public void testExisteFils() {
+		
+		Noeud noeud = new Noeud(new Pion(new Point(0,0)));
+		
+		assertFalse(noeud.existeFils());
+		noeud.AjouterFils(new Noeud(new Pion(new Point(0,1))));	
+		assertTrue(noeud.existeFils());
+		
+	}
+
+}
