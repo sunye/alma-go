ab5290acd9746af273effbcf335b15e8e6045f8d
alexandre.garnier.86 <alexandre.garnier.86@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-15 07:24:34 +0000

    Fuckin' running hellish project \o/

--- GroupeM/trunk/src/fr/alma/go/ui/GoFrame.java
+++ GroupeM/trunk/src/fr/alma/go/ui/GoFrame.java
@@ -1,6 +1,5 @@
 package fr.alma.go.ui;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,40 +7,72 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-import fr.alma.go.Game;
+import fr.alma.go.GameManager;
 
 public class GoFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
-	private GoPanel goPan;
-	
-	private Game game;
-	
 	private JMenuBar barreMenu = new JMenuBar();
 	private JMenu jeu = new JMenu("Let's go play go !");
-	private JMenu nouv = new JMenu("New game");
-	private JMenuItem item1 = new JMenuItem("Human vs CPU");
-	private JMenuItem item2 = new JMenuItem("Human vs Human");
-	private JMenuItem item3 = new JMenuItem("Alien vs Predator");
-	private JMenuItem quitter = new JMenuItem("Leave me alone !");
+	private JMenu a_propos = new JMenu("About");
+	private JMenu nouv = new JMenu("Enter the matrix");
+	private JMenuItem item1 = new JMenuItem("Human versus CPU");
+	private JMenuItem item2 = new JMenuItem("Human versus Human (why the hell don't you play IRL ??)");
+	private JMenuItem propos = new JMenuItem("?");
+	private JMenuItem quitter = new JMenuItem("Take the blue pill");
+	private OptionWindow options;
+	// Panel représentant le goban
+	private GoPanel pan;
+	private boolean sendData;
+	private GameManager controleur;
 
-	public GoFrame() {
-		this.setTitle("Atari Go");
-		this.setSize(600, 600); // 4;23
+	/**
+	 * Open a window with the goban and a menu
+	 * 
+	 * @param s
+	 *            : Title of the window
+	 */
+	public GoFrame(String s) {
+
+		super(s);
+		setSize(750, 680);
 		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		
-		game=new Game();
 
-		this.setBackground(new Color(105, 155, 105));
-		goPan = new GoPanel(game);
-		goPan.enableClick();
-		this.setContentPane(goPan);
-		this.addMouseListener(goPan.getClick());
+		pan = new GoPanel();
+		/**
+		 * On ajoute le GobanPanel dans la fenetre principale
+		 */
+		add(pan);
+		controleur = new GameManager(pan, this);
 
+		/**
+		 * Mise en place de la barre de menu
+		 */
+		// Un joueur :
+		item1.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				OptionWindow ow = new OptionWindow(null,
+						"Choisissez votre couleur !", true, controleur);
+			}
+		});
+		this.nouv.add(item1);
+
+		// Deux joueurs :
+		item2.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				controleur.twoPlayers();
+			}
+		});
+		this.nouv.add(item2);
+		this.jeu.add(this.nouv);
+		// Ajout d'un separateur
+		this.jeu.addSeparator();
+
+		// Quitter :
 		quitter.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
@@ -49,16 +80,48 @@ public class GoFrame extends JFrame {
 			}
 		});
 
-		nouv.add(item1);
-		nouv.add(item2);
-		nouv.add(item3);
-		jeu.add(nouv);
-		jeu.addSeparator();
-		jeu.add(quitter);
-		barreMenu.add(jeu);
-		setJMenuBar(barreMenu);
+		this.jeu.add(quitter);
+		this.barreMenu.add(jeu);
 
+		// TODO : changer le "Bla bla bla..." du "A propos" !
+		// A propos :
+		propos.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane jop = new JOptionPane();
+				jop
+						.showMessageDialog(
+								null,
+								"Matrix go interface, all credits to the great architect\n\t© Julien Durillon & ClotHilde Massot",
+								"About", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		this.a_propos.add(propos);
+		this.barreMenu.add(a_propos);
+
+		this.setJMenuBar(barreMenu);
 		this.setVisible(true);
-	} // GoFrame()
 
-} // class GoFrame
+		/**
+		 * Arrêt de l'appli quand on clique sur la croix
+		 */
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	}
+
+	/**
+	 * Open a window endgame to announce the winner of the game.
+	 */
+	public void finJeu() {
+		JOptionPane jop = new JOptionPane();
+		StringBuilder strb = new StringBuilder();
+		strb.append("Le grand gagnant de cette partie est : Joueur ");
+		if (controleur.getGame().getWhiteTaken() > controleur.getGame()
+				.getBlackTaken())
+			strb.append("Noir !");
+		else
+			strb.append("Blanc !");
+		jop.showMessageDialog(null, strb, "Partie terminée !",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}
