1397e6ff9175b827dc65ff8a6a84602d4eb43539
julien.durillon <julien.durillon@fe6e65f2-260b-11df-8e73-9931e1e0996c>
2010-04-19 13:53:59 +0000

    fonts

--- groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/utils/StoneVal.java
+++ groupeE/trunk/atarigo/src/main/java/fr/alma/atarigo/utils/StoneVal.java
@@ -16,8 +16,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// along with this program;
 //
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of this program.
